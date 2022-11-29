5ec0336d185bf8b7b90d22cd96ac5f3d270aedc6
MandyKrason <Mandy.Krason@ausbildung.lavie-reha.de>
2022-11-29 12:27:22 +0100

    LoginPage unter dem neuen Framework

---
+++ UserInterface/UserInterface/LoginPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserInterface
+{
+    /// <summary>
+    /// Interaktionslogik für LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Click_Login(object sender, RoutedEventArgs e)
+        {
+            // TODO: Methode aus Login-Bibliothek verwenden
+            /*
+            if (true) {
+                return true;
+            }
+            
+
+            return false;*/
+
+            Login(username.Text, password.Text);
+        }
+
+        public void Login(string username, string password)
+        {
+            // TODO: Login
+        }
+
+    }
+}
