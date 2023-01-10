e53c3613ce581501e1ff3e4250c9f746f50b0165
Thore Braun <tjb@mailbox.org>
2023-01-10 09:31:36 +0100

    requestmodel erstellt

--- Tennisclubmanagment/App/RegisterUser/RegisterUserInteractor.cs
+++ Tennisclubmanagment/App/RegisterUser/RegisterUserInteractor.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace App.RegisterUser
 {
-    internal class RegisterUserInteractor
+    internal class RegisterUserInteractor : IRegisterUserRequestInterface
     {
+        RegisterUserRequestmodel _requestmodel;
+        RegisterUserResponsmodel _responsmodel;
+
+        public void RegisterUser(
+            string username,
+            string password,
+            string firstName,
+            string middelNames,
+            string lastName,
+            string street,
+            int houseNo,
+            string postCode,
+            string city,
+            string iban)
+        {
+            RegisterUserRequestmodel = new RegisterUserRequestmodel(string username,
+            string password,
+            string firstName,
+            string middelNames,
+            string lastName,
+            string street,
+            int houseNo,
+            string postCode,
+            string city,
+            string iban);
+        }
     }
 }
--- Tennisclubmanagment/App/RegisterUser/RegisterUserRequestModel.cs
+++ Tennisclubmanagment/App/RegisterUser/RegisterUserRequestModel.cs
@@ -8,5 +8,40 @@ namespace App.RegisterUser
 {
     internal class RegisterUserRequestmodel
     {
+        public string Username { get; }
+        public string Password { get; }
+        public string FirstName { get; }
+        public string MiddelNames { get; }
+        public string LastName { get; }
+        public string Street { get; }
+        public int HouseNo { get; }
+        public string PostCode { get; }
+        public string Iban { get; }
+        
+        public RegisterUserRequestmodel(
+           string username,
+           string password,
+           string firstName,
+           string middelNames,
+           string lastName,
+           string street,
+           int houseNo,
+           string postCode,
+           string city,
+           string iban)
+        {
+            Username = username;
+            Password = password;
+            FirstName = firstName;
+            MiddelNames = middelNames;
+            LastName = lastName;
+            Street = street;
+            HouseNo = houseNo;
+            PostCode = postCode;
+            City = city;
+            Iban = iban;
+        }
+
+
     }
 }
